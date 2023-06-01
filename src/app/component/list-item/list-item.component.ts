1d6aa4aec1d221b1ae7ca4ce880ebad6c5918098
rballesteros <rballesteros@ayesa.com>
2023-06-01 14:01:21 +0200

    feat: poke-list-item

--- src/app/component/list-item/list-item.component.ts
+++ src/app/component/list-item/list-item.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list-item',
   templateUrl: './list-item.component.html',
-  styleUrls: ['./list-item.component.css']
+  styleUrls: ['./list-item.component.css'],
 })
 export class ListItemComponent {
-
+  title = 'pokeapi';
+  nombre = 'Prueba';
+  imagen = '../assets/images/logo.png';
+  id = 1;
 }
