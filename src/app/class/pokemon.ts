c65a449a7fc5b491d17100240f29d40b6b8493a4
Corco1000 <acorcoles@ayesa.com>
2023-06-01 13:22:52 +0200

    clase pokemon actualizada

--- src/app/class/pokemon.ts
+++ src/app/class/pokemon.ts
@@ -1,2 +1,12 @@
 export class Pokemon {
+    constructor(
+        public id:number, 
+        public name:string, 
+        public base_experience:number, 
+        public height:number, 
+        public is_default:boolean, 
+        public order:number, 
+        public weight:number ) {
+
+    }
 }
