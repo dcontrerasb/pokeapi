228913136f125a0ceabcc906ac83264a2005581f
Corco1000 <acorcoles@ayesa.com>
2023-06-02 11:33:57 +0200

    pokemon detalles

--- src/app/component/pokemon-item/pokemon-item.component.ts
+++ src/app/component/pokemon-item/pokemon-item.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pokemon } from 'src/app/class/pokemon';
+import { Sprites } from 'src/app/class/sprites';
+import { PokemonService } from 'src/app/services/pokemon.service';
+
+
 
 @Component({
   selector: 'app-pokemon-item',
@@ -6,5 +12,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-item.component.css']
 })
 export class PokemonItemComponent {
+  
+  pokemon:string|null="";
+  pokemones:Pokemon[]=[];
 
+  constructor(private pokemonService: PokemonService, private route: ActivatedRoute) {
+
+    this.pokemon=route.snapshot.paramMap.get('nombre');
+
+    if(this.pokemon != null){
+      //Comprobar que no sea nulo
+      this.pokemonService.getPokemon(this.pokemon).subscribe((res) => {
+        this.pokemones.push(res);
+      });
+    }
+  }
 }
