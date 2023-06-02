5fc1bd119eb4be255f8a0ce11aae2bd4a2bdc151
rballesteros <rballesteros@ayesa.com>
2023-06-02 11:35:28 +0200

    fix: fixed browser

--- src/app/component/search/search.component.ts
+++ src/app/component/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Pokemon } from 'src/app/class/pokemon';
 import { Sprites } from 'src/app/class/sprites';
 import { PokemonService } from 'src/app/services/pokemon.service';
@@ -11,19 +11,22 @@ import { PokemonService } from 'src/app/services/pokemon.service';
 })
 export class SearchComponent {
   pokemones: Pokemon[] = [];
-  pokemon: string = "";
-  pokemonNombre:string | null = "";
+  pokemon: string = '';
+  pokemonNombre: string | null = '';
 
-  constructor(private pokemonService: PokemonService, private route: ActivatedRoute) {
-    this.pokemonNombre=route.snapshot.paramMap.get('nombre');
+  constructor(
+    private pokemonService: PokemonService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.pokemonNombre = route.snapshot.paramMap.get('nombre');
 
-    if(this.pokemonNombre != null && this.pokemonNombre != "0"){
+    if (this.pokemonNombre != null) {
       //Respuesta para obtener un solo pokemon
       this.pokemonService.getPokemon(this.pokemonNombre).subscribe((res) => {
         this.pokemones.push(res);
       });
-    }
-    else{
+    } else {
       //Respuesta para obtener varios pokemon
       this.pokemonService.getPokemons().subscribe((res) => {
         res.results.forEach((pokemon: Pokemon) => {
@@ -31,6 +34,16 @@ export class SearchComponent {
             this.pokemones.push(res);
           });
         });
+      });
+    }
+  }
+
+  searchPokeon(event: String) {
+    if (event === '' || event === null || event === '0') {
+      this.router.navigate(['']);
+    } else {
+      this.router.navigate(['']).then(() => {
+        this.router.navigate(['', event]);
       });
     }
   }
